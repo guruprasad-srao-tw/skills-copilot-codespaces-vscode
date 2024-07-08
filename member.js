69bfe9c388160aca755f71b1823cc479e12f30af
guruprasad-srao-tw <120443720+guruprasad-srao-tw@users.noreply.github.com>
2024-07-08 06:42:05 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,65 @@
+function skillsMember() {
+  var member = document.getElementById('member').value;
+  var member = member.toLowerCase();
+  var member = member.replace(/[^a-zA-Z ]/g, "");
+  var member = member.trim();
+  var member = member.split(" ");
+  var member = member.filter(function(value, index, arr) {
+    return value !== "";
+  });
+  var member = member.join(" ");
+  var member = member.split(" ");
+  var member = member.filter(function(value, index, arr) {
+    return value !== "";
+  });
+  var member = member.join(" ");
+  var member = member.split(" ");
+  var member = member.filter(function(value, index, arr) {
+    return value !== "";
+  });
+  var member = member.join(" ");
+  var member = member.split(" ");
+  var member = member.filter(function(value, index, arr) {
+    return value !== "";
+  });
+  var member = member.join(" ");
+  var member = member.split(" ");
+  var member = member.filter(function(value, index, arr) {
+    return value !== "";
+  });
+  var member = member.join(" ");
+  var member = member.split(" ");
+  var member = member.filter(function(value, index, arr) {
+    return value !== "";
+  });
+  var member = member.join(" ");
+  var member = member.split(" ");
+  var member = member.filter(function(value, index, arr) {
+    return value !== "";
+  });
+  var member = member.join(" ");
+  var member = member.toLowerCase();
+  var member = member.replace(/[^a-zA-Z ]/g, "");
+  var member = member.trim();
+  var member = member.split(" ");
+  var member = member.filter(function(value, index, arr) {
+    return value !== "";
+  });
+  var member = member.join(" ");
+  var member = member.split(" ");
+  var member = member.filter(function(value, index, arr) {
+    return value !== "";
+  });
+  var member = member.join(" ");
+  var member = member.split(" ");
+  var member = member.filter(function(value, index, arr) {
+    return value !== "";
+  });
+  var member = member.join(" ");
+  var member = member.split(" ");
+  var member = member.filter(function(value, index, arr) {
+    return value !== "";
+  });
+  var member = member.join(" ");
+  var member = member.split(" ");
+  var member = member.filter(function(value, index, arr
